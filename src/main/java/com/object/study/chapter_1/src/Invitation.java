397ae31f4fa23a5bb1ff64ed8346c987f5e097a1
hong9Lee <hongtemp9mac@gmail.com>
2023-10-11 22:12:11 +0900

    init

---
+++ src/main/java/com/object/study/chapter_1/src/Invitation.java
@@ -0,0 +1,7 @@
+package com.object.study.chapter_1.src;
+
+import java.time.LocalDateTime;
+
+public class Invitation {
+    private LocalDateTime when;
+}
---
+++ src/main/java/com/object/study/chapter_1/src/Ticket.java
@@ -0,0 +1,9 @@
+package com.object.study.chapter_1.src;
+
+public class Ticket {
+    private Long fee;
+
+    public Long getFee() {
+        return fee;
+    }
+}
