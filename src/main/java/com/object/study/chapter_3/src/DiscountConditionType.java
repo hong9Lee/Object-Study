f6d23030f87f9b089e370748da2c673f04e7017f
hong9Lee <hongtemp9mac@gmail.com>
2023-10-29 16:46:32 +0900

    4장 설계 품질과 트레이드 오프 정리

---
+++ src/main/java/com/object/study/chapter_3/src/DiscountConditionType.java
@@ -0,0 +1,6 @@
+package com.object.study.chapter_3.src;
+
+public enum DiscountConditionType {
+    SEQUENCE,
+    PERIOD
+}
---
+++ src/main/java/com/object/study/chapter_3/src/MovieType.java
@@ -0,0 +1,7 @@
+package com.object.study.chapter_3.src;
+
+public enum MovieType {
+    AMOUNT_DISCOUNT, // 금액 할인 정책
+    PERCENT_DISCOUNT, // 비율 할인 정책
+    NONE_DISCOUNT // 미적용
+}
